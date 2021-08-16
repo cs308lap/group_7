0b276b02c96cc10e9618704af4a9d3f1bb3529c9
PrajwalSood <prajwalsood@gmail.com>
2021-08-16 15:44:42 +0530

    Added comments

--- factorial.cpp
+++ factorial.cpp
@@ -7,8 +7,8 @@ int factorial(int n){
     if (n<0){
         cout << n << " is not a positive integer \n";
     }
-    if(n!=1){
-	return(n * factorial(n-1));
+    if(n!=1){ 
+	return(n * factorial(n-1)); // Recursive call to calculate factorial
     }
-    else return 1;
+    else return 1; // Base case
 }
