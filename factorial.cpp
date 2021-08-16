38da5df4d421f81292cda110aa8a693993da71fc
PrajwalSood <prajwalsood@gmail.com>
2021-08-16 15:36:55 +0530

    Added negative handling in factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,12 @@
 #include "functions.h"
+#include <iostream>
+
+using namespace std;
 
 int factorial(int n){
+    if (n<0){
+        cout << n << " is not a positive integer \n";
+    }
     if(n!=1){
 	return(n * factorial(n-1));
     }
