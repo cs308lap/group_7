1355f9492445830c7dbda4854be9e407796a04db
Priyanshu-Kumar-Singh <priyanshu14012002@gmail.com>
2021-08-30 08:40:19 +0530

    Added recursive func to find gcd of two numbers

--- gcd.cpp
+++ gcd.cpp
@@ -3,11 +3,8 @@
 using namespace std;
 
 int gcd(int a,int b){
-    if(a==0)return b;
-    if(b==0)return a;
-    while(a!=b){
-        if(a>b)a-=b;
-        else b-=a;
+    if(b==0){
+        return a;
     }
-    return a;
+    return gcd(b,a%b);
 }
